c7e513623ae7124a25ad04d16d172d997dcac750
SawyerChristensen <schr5498@gmail.com>
2026-06-23 14:23:38 -0700

    Initial Commit

---
+++ EagleEye/EagleEyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  EagleEyeApp.swift
+//  EagleEye
+//
+//  Created by Sawyer Christensen on 6/23/26.
+//
+
+import SwiftUI
+
+@main
+struct EagleEyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ EagleEyeTests/EagleEyeTests.swift
@@ -0,0 +1,19 @@
+//
+//  EagleEyeTests.swift
+//  EagleEyeTests
+//
+//  Created by Sawyer Christensen on 6/23/26.
+//
+
+import Testing
+@testable import EagleEye
+
+struct EagleEyeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
